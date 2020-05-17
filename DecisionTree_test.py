26cc782e9c7211948797245da949143aee38e764
qaz57301@163.com <qaz57301@163.com>
2020-05-17 22:19:44 +0800

    update

--- DecisionTree_test.py
+++ DecisionTree_test.py
@@ -4,53 +4,36 @@ import pandas as pd
 
 
 def info_entry(p):
-    return -1*sum(p*np.log10(p))
+    return -1*sum(p*np.log2(p))
 
 #计算信息增益
 def gain_entry(feat_data):
     class_gain = info_entry(list(feat_data.iloc[:,-1].value_counts(normalize=True)))
-    #ent_dict存放每个特征的概率字典
+    columns = feat_data.columns
+    labels = columns[-1]              #datafrmae最后一列
+    category = list(set(feat_data[labels]))       ##将最后一列去重获得类别
+    col_labs = columns[0:-1]          #datafrmae去掉最后一列
+    ent_dict = {}    #存放各特征信息增益值
+    m = feat_data.shape[0]
+    #ent_dict存放每个特征的信息增益
     ent_dict = {}
-    for col in feat_data.columns:
-        #ent_dict[col]存放每个特征里面每个取值的概率
-        ent_dict[col]={}
-        ent_dict[col]=info_entry(list(feat_data[col].value_counts(normalize=True)))
-        class_gain-=ent_dict[col]
-    return class_gain
+    for col in col_labs:
+        #每一列的信息增益
+        ent_dict[col] = {}
+        p_lab = []
+        for i in list(set(feat_data[col])):
+            #每个特征中的每个取值的长度
+            n = len(feat_data[feat_data[col] == i])
+            for lab in category:
+                #每个特征每个取值对应不同分类的概率
+                p = len(feat_data[(feat_data[col] == i) & (feat_data[labels] == lab)]) / len(
+                    feat_data[feat_data[col] == i])
+                if p > 0:
+                    p1 = -1 * (n / m) * p * np.log2(p)
+                else:
+                    p1 = 0
+                p_lab.append(p1)
+        ent_dict[col] = class_gain - sum(p_lab)
+    node = max(ent_dict, key=ent_dict.get)
+    return node
 
-#计算信息增益
-def gain_entry(feat_data):
-    y=feat_data.iloc[:, -1]
-    category= list(set(feat_data.iloc[:, -1]))
-    # for label in category:
-    #     p = len([label_ for label_ in category if label_==label])/len(y)
-    #ent_dict存放每个特征的概率字典
-    # ent_dict = {}
-    for lab in category:
-        for col in feat_data.columns:
-            # ent_dict[col]={}
-            # ent_dict[col]存放每个特征里面每个取值的概率
-            c_lab = list(set(feat_data[col]))
-            ent_dict = len(feat_data[col]==c_lab&feat_data.iloc[:, -1]==lab)/len(feat_data[col]==c_lab)
-
-
-
-    return class_gain
-
-
-
-path = 'E:/tianchi/data/decisiontreedata/'
-file_name = 'decisitontree.csv'
-feat_data = pd.read_csv(path+file_name,encoding='gbk')
-y = feat_data.iloc[:, -1]
-category = list(set(feat_data.iloc[:, -1]))
-# for label in category:
-#     p = len([label_ for label_ in category if label_==label])/len(y)
-# ent_dict存放每个特征的概率字典
-# ent_dict = {}
-for lab in category:
-    for col in feat_data.columns:
-        # ent_dict[col]={}
-        # ent_dict[col]存放每个特征里面每个取值的概率
-        c_lab = list(set(feat_data[col]))
-        ent_dict = len(feat_data[col] == c_lab & feat_data.iloc[:, -1] == lab) / len(feat_data[col] == c_lab)
